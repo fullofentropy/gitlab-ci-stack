a54129a9ca4e218dfc7fc4962857d95f50f6e9f7
Richard_Boettcher <richard.boettcher@alumni.purdue.edu>
2022-04-04 16:09:21 -0500

    making my own mods

---
+++ server.py
@@ -0,0 +1,31 @@
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+# open an url like http://127.0.0.1/example the method do_GET() i
+
+hostName = "0.0.0.0"
+serverPort = 8880
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Sample Webserver</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server, if you see this, the container succeeded.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
